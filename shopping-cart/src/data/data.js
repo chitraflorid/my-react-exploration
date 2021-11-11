e8c9179f05531253a8d97ca6487fe9410219fa7c
chitra <chitraflorid@users.noreply.github.com>
2021-11-11 12:29:09 +0530

    Create data.js

---
+++ shopping-cart/src/data/data.js
@@ -0,0 +1,22 @@
+const SHOPPING_CART_ITEMS = [
+  {
+    id: "1",
+    name: "Eggs",
+    price: 2.99,
+    quantity: 1,
+  },
+  {
+    id: "2",
+    name: "Milk",
+    price: 1.99,
+    quantity: 2,
+  },
+  {
+    id: "3",
+    name: "Cheese",
+    price: 3.99,
+    quantity: 3,
+  },
+];
+
+export {SHOPPING_CART_ITEMS as items};
