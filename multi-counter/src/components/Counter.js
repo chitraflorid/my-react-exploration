a5eefddc72f9bc2f20500542bd6d2202e7fef971
chitra <chitraflorid@users.noreply.github.com>
2021-11-11 13:46:01 +0530

    Create Counter.js

---
+++ multi-counter/src/components/Counter.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+// Counter Component
+class Counter extends Component {  
+  constructor(props) {
+    super(props);
+  }
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.currentActive === this.props.indx;
+  }
+  
+  render() {
+    const { indx, value, onIncrement, onDecrement } = this.props;
+    return (
+      <div className="counter">
+        <b>{value}</b>
+        <div className="counter-controls">
+          <button onClick={() => onDecrement( 1)} className="button is-danger is-small">-</button>
+          <button onClick={()=> onIncrement( indx + 1)} className="button is-success is-small">+</button>
+        </div>
+      </div>
+    );
+  }
+}
