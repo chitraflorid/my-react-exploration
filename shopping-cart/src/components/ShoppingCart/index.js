3a21b4db39dac7ba85f58ad51a5931515e8c4c7f
chitra <chitraflorid@users.noreply.github.com>
2021-11-11 12:30:02 +0530

    Create index.js

---
+++ shopping-cart/src/components/ShoppingCart/index.js
@@ -0,0 +1,7 @@
+import {ShoppingCart} from './ShoppingCart';
+import {ShoppingCartProvider} from './ShoppingCartProvider';
+import  {ShoppingCartContext}  from './context/shoppingCartContext';
+
+
+export {ShoppingCart, ShoppingCartProvider, ShoppingCartContext};
+    
