7ff0e4c89fbd9bf7b2924e4454b58a567c4f3704
chitra <chitraflorid@users.noreply.github.com>
2021-11-11 13:13:29 +0530

    Create App.js

---
+++ accordion/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordion from './components/Accordion';
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
