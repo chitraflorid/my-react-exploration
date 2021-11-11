d7d1c1e4288a12ab20576ac20d07cfa1b8d0278b
chitra <chitraflorid@users.noreply.github.com>
2021-11-11 13:25:29 +0530

    Add files via upload

---
+++ accordion/src/components/AccordionTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AccordionTitle = (({title, className}) => {
+  
+    return (
+        
+            <h2 className={className}>{title}</h2>
+     
+    );
+});
+                         
+export default AccordionTitle;
